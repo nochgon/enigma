d6290f2e01fb2d7022c9a13f9855f395fe0ac019
nochgon <nochgon@gmail.com>
2021-07-24 14:02:00 +0900

    create_randomを実装(EnigmaFactory)

--- enigma/enigma_factory.py
+++ enigma/enigma_factory.py
@@ -10,17 +10,26 @@ class EnigmaFactory:
     }
 
     @classmethod
-    def create(cls, command: str, len_scrambers: int,
-               seeds_scramber: Tuple[Any, ...] = None,
-               seed_reflector: Any = None
-               ) -> enigma.Enigma:
+    def create_random(cls, command: str, len_scrambers: int,
+                      seeds_scramber: Tuple[Any, ...] = None,
+                      seed_reflector: Any = None
+                      ) -> enigma.Enigma:
         if command not in cls.__map_command_size:
             raise ValueError(f'commandが不正: {command}')
-
+        # encoderを先に生成。サイズの情報を持っているため。
         encoder = parts.EncoderFactory.create(command)
-        plug = parts.PlugLink.create()
-        drum = parts.Drum.create(cls.__map_command_size[command],
-                                 len_scrambers,
-                                 seeds_scramber=seeds_scramber,
-                                 seed_reflector=seed_reflector)
+
+        # 乱数シードの指定があるならそれを使って生成
+        scrambers = [
+            parts.ScramberFactory.create_random(encoder.size, seed)
+            for seed in seeds_scramber
+        ] if seeds_scramber else [
+            parts.ScramberFactory.create_random(encoder.size)
+            for i in range(len_scrambers)
+        ]
+        reflector = parts.ReflectorFactory.create_random(encoder.size,
+                                                         seed_reflector)
+
+        drum = parts.Drum(scrambers, reflector)
+        plug = parts.PlugLink()
         return enigma.Enigma(drum, plug, encoder)
